6a31727d17e4e4f1f8c40e35f75f52d9eeaa1a13
zsenarchitect <61437205+zsenarchitect@users.noreply.github.com>
2024-04-17 10:30:46 -0400

    Auto push changes committed on 2024-04-17...1

--- hooks/command-before-exec[ID_KEYNOTE_USER].py
+++ hooks/command-before-exec[ID_KEYNOTE_USER].py
@@ -10,14 +10,14 @@ doc = args.ActiveDocument
 @EnneadTab.ERROR_HANDLE.try_catch_error_silently
 def main():
 
-    if doc.IsFamilyDocument:
-        return
+    # if doc.IsFamilyDocument:
+    #     return
     
-    if EnneadTab.USER.get_user_name() == "yumeng.an" and EnneadTab.TIME.get_YYYYMMDD() == "231215":
-        return
-    if EnneadTab.USER.get_user_name() == "hjlee" and EnneadTab.TIME.get_YYYYMMDD() < "240315":
+    # if EnneadTab.USER.get_user_name() == "yumeng.an" and EnneadTab.TIME.get_YYYYMMDD() == "231215":
+    #     return
+    # if EnneadTab.USER.get_user_name() == "hjlee" and EnneadTab.TIME.get_YYYYMMDD() < "240315":
         
-        return
+    #     return
     EnneadTab.NOTIFICATION.duck_pop(main_text = "EnneaDuck dislikes [UserKeynote], that tag will not link to other database! Quack!\nOnly use [UserKeynote] when you have ABSOLUTELY no choice.")
     print("this duck is enforced and will not be turned off by setting.")
     args.Cancel = False
