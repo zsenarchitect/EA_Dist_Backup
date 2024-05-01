4bc39a9003e1a17a6b0d999426023979a1785b05
zsenarchitect <61437205+zsenarchitect@users.noreply.github.com>
2024-05-01 17:12:09 -0400

    Auto push changes committed on 2024-05-01...3

--- hooks/command-before-exec[ID_BUTTON_DELETE].py
+++ hooks/command-before-exec[ID_BUTTON_DELETE].py
@@ -12,7 +12,47 @@ uiapp = UI.UIApplication(doc.Application)
 
 @EnneadTab.ERROR_HANDLE.try_catch_error_silently
 def main():
+    check_depedent_views()
+    check_rvt_link()
+
+
+def check_rvt_link():
+    selection_ids = uidoc.Selection.GetElementIds()
+    selection = [doc.GetElement(x) for x in selection_ids]
+   
+    def is_rvt_link(x):
+        if not hasattr(x, 'MoveBasePointToHostBasePoint'):
+            return False
+        return True
     
+
+    links = filter(is_rvt_link, selection)
+   
+    
+    if len(links) == 0: 
+        return
+    
+    options = ["Oops! Do not delete selected link(s).",
+               "Just DELETE selected link(s)."]
+    
+    note = "Are you sure you want to delete revit link?"
+    for link in links:
+        note +=  "\n  + " + link.GetLinkDocument ().Title
+        
+    res = EnneadTab.REVIT.REVIT_FORMS.dialogue(main_text = "There are revit links in current selection.",
+                                                sub_text = note,
+                                                options = options)
+    if res == options[0]:
+        args.Cancel = True
+    if res == options[1]:
+        args.Cancel = False
+    else:
+        args.Cancel = True
+
+
+
+        
+def check_depedent_views():
     selection_ids = uidoc.Selection.GetElementIds()
     selection = [doc.GetElement(x) for x in selection_ids]
     # for x in EnneadTab.REVIT.REVIT_SELECTION.get_selection():
