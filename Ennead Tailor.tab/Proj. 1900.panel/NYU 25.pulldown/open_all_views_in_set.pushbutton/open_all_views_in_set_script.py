2cb5d5525add5c021f67ae1a0e27d50c09faea41
zsenarchitect <61437205+zsenarchitect@users.noreply.github.com>
2024-05-17 11:00:01 -0400

    Auto push changes committed on 2024-05-17...1

---
+++ Ennead Tailor.tab/Proj. 1900.panel/NYU 25.pulldown/open_all_views_in_set.pushbutton/open_all_views_in_set_script.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+__doc__ = "Sen Zhang has not writed documentation for this tool, but he should!"
+__title__ = "Open Favorite Views by Set"
+
+from pyrevit import forms #
+from pyrevit import script #
+
+import ENNEAD_LOG
+from EnneadTab import ERROR_HANDLE, NOTIFICATION
+from EnneadTab.REVIT import REVIT_APPLICATION, REVIT_VIEW
+from Autodesk.Revit import DB 
+# from Autodesk.Revit import UI
+# uidoc = EnneadTab.REVIT.REVIT_APPLICATION.get_uidoc()
+doc = REVIT_APPLICATION.get_doc()
+
+@ERROR_HANDLE.try_catch_error
+def open_all_views_in_set():
+    all_view_sheet_sets = DB.FilteredElementCollector(doc).OfClass(DB.ViewSheetSet).ToElements()
+
+
+
+    # let user pick multuple sheetsets
+    selected_print_sets = forms.SelectFromList.show(all_view_sheet_sets,
+                                                    multiselect=True,
+                                                    name_attr='Name',
+                                                    button_name='Select Print Sets',
+                                                    title = "Who are the next sexy views to open?")
+    if not selected_print_sets:
+        return
+
+    for print_set in selected_print_sets:
+        for i, view in enumerate(print_set.Views):
+            NOTIFICATION.messenger("Openning <{}/{}>[{}] in the background, hold on...".format(i, len(list(print_set.Views)), view.Name))
+            try:
+                REVIT_APPLICATION.get_uidoc().ActiveView = view
+            except Exception as e:
+                NOTIFICATION.messenger(str(e))
+
+
+
+################## main code below #####################
+
+
+if __name__ == "__main__":
+    output = script.get_output()
+    output.close_others()
+    open_all_views_in_set()
+    ENNEAD_LOG.use_enneadtab(coin_change = 20, tool_used = __title__.replace("\n", " "), show_toast = True)
+
+
+
+
+
+
+
