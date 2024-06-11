1393c9d22c641e336786ed375757a971cf6d4e6b
zsenarchitect <61437205+zsenarchitect@users.noreply.github.com>
2024-06-10 21:07:55 -0400

    Auto push changes committed on 2024-06-10...9

--- _revit/ENNEAD.extension/lib/EnneadTab/REPO MANAGER/publish_both.py
+++ _revit/ENNEAD.extension/lib/EnneadTab/REPO MANAGER/publish_both.py
@@ -7,9 +7,9 @@ import VERSION_CONTROL
 def publish_both():
     start = time.time()
     sync_core_module.sync_core()
-    VERSION_CONTROL.copy_to_dist_folder()
     publish_revit.publish_revit()
     publish_rhino.publish_rhino()
+    VERSION_CONTROL.copy_to_dist_folder()
 
     end_time = time.time()
     print ("\n\n\nFinish in {}s".format(end_time - start))
