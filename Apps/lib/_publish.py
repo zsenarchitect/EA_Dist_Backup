7d818acf0bd460045dce8db42cec6e193eab455e
zsenarchitect <61437205+zsenarchitect@users.noreply.github.com>
2024-06-22 19:55:41 -0400

    Auto push changes committed on 2024-06-22...4

--- Apps/lib/_publish.py
+++ Apps/lib/_publish.py
@@ -47,7 +47,7 @@ def publish():
 
     # Play Windows built-in notification sound
     winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-    winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
+    winsound.MessageBeep(winsound.MB_ICONHAND)
 
 
 def get_nth_commit_number():
