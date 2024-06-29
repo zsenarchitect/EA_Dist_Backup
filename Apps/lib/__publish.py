f2e628b92f2a807d0a5d8f16a784615f6681a69d
zsenarchitect <61437205+zsenarchitect@users.noreply.github.com>
2024-06-29 14:53:20 -0400

    Auto push changes committed on 2024-06-29...7

--- Apps/lib/__publish.py
+++ Apps/lib/__publish.py
@@ -178,7 +178,8 @@ def update_installer_folder():
 
     # copy folder from current repo to EA_dist repo
     for file in ["EnneadTab_OS_Installer.exe",
-                 "EnneadTab_For_Revit(Legacy)_Installer.exe"]:
+                 "EnneadTab_For_Revit(Legacy)_Installer.exe",
+                 "EnneadTab_For_Revit_UnInstaller.exe"]:
         shutil.copy(os.path.join(current_repo_folder, "Apps", "lib", "exes", "products", file), 
                     os.path.join(current_repo_folder, "Installation", file))
 
