c18c2fa03969e050b4a8c3599d5ee9149e8ce058
zsenarchitect <61437205+zsenarchitect@users.noreply.github.com>
2024-07-05 17:09:27 -0400

    Auto push changes committed on 2024-07-05...14

---
+++ Apps/lib/EnneadTab/RHINO/Enneaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaad.menu/Installation.button/Installation_left.py
@@ -0,0 +1,11 @@
+
+__alias__ = "Install EnneadTab"
+__doc__ = "Install EnneadTab"
+
+
+def Installation():
+    print ("To be created")
+
+    # make a local copy of main rui and close installer rui, open main rui
+
+    # register alias, including the starter
--- Apps/lib/EnneadTab/RHINO/Enneaaaaaaaaaaaaaaaaaaaaaaaaaaad.menu/Installation.button/Installation_left.py
+++
@@ -1,7 +0,0 @@
-
-__alias__ = "Install EnneadTab"
-__doc__ = "Install EnneadTab"
-
-
-def Installation():
-    print (123)
