a301f87a60e4ce40389cb68592e1a2bd5aa964e7
zsenarchitect <61437205+zsenarchitect@users.noreply.github.com>
2024-07-24 16:11:44 -0400

    remove

--- Ennead+.tab/Resource.panel/archive/websites_external-stack/Benjamin_Moore.pushbutton/script.py
+++
@@ -1,9 +0,0 @@
-from pyrevit import forms
-from pyrevit import script
-
-__title__ = 'Benjamin\nMoore'
-__context__ = 'zero-doc'
-__doc__ = 'Find good color pallete'
-
-
-script.open_url("https://www.benjaminmoore.com/en-us/paint-colors/color-families")
--- lib/EnneadTab/REVIT/REVIT_UPDATER.py
+++
@@ -1,6 +0,0 @@
-"""not to be confused with the repo updater for updating by git
-
-
-https://discourse.pyrevitlabs.io/t/dynamic-model-updater-and-revit-events/1593
-here is a good example of using the updater to register a customized event
-"""
