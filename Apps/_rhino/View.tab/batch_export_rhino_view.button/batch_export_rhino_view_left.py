2a16269a931a7cf2a89ddf1993e755de1745a385
Sen Zhang <zsenarchitect@gmail.com>
2024-09-25 17:32:38 -0400

    Auto Commit 2024-09-25_17-32-38

--- Apps/_rhino/View.tab/batch_export_rhino_view.button/batch_export_rhino_view_left.py
+++ Apps/_rhino/View.tab/batch_export_rhino_view.button/batch_export_rhino_view_left.py
@@ -4,6 +4,7 @@ __doc__ = "This button does BatchExportRhinoView when left click"
 
 import rhinoscriptsyntax as rs
 from EnneadTab import ERROR_HANDLE, LOG
+from EnneadTab.RHINO import RHINO_FORMS
 
 @LOG.log(__file__, __title__)
 @ERROR_HANDLE.try_catch_error()
@@ -20,8 +21,11 @@ def batch_export_rhino_view():
         return
     
     all_views = rs.NamedViews()
+    selected_views = RHINO_FORMS.select_from_list(sorted(all_views, reverse=True), message="Select Views to Export", button_names=["Export"])
+    if not selected_views:
+        return
 
-    for view in sorted(all_views, reverse=True):
+    for view in selected_views:
         print("Exporting view [{}]".format(view))
         rs.RestoreNamedView(view, view=None, restore_bitmap=False)
         file_path = "{}\\{}.{}".format(folder, view, format)
