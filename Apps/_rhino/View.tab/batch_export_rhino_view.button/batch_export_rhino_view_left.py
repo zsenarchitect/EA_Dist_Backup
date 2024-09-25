abdd6ff15fd32d01d7698edee51314781a44c2b8
Sen Zhang <zsenarchitect@gmail.com>
2024-09-25 17:19:31 -0400

    Auto Commit 2024-09-25_17-19-31

---
+++ Apps/_rhino/View.tab/batch_export_rhino_view.button/batch_export_rhino_view_left.py
@@ -0,0 +1,31 @@
+
+__title__ = "BatchExportRhinoView"
+__doc__ = "This button does BatchExportRhinoView when left click"
+
+import rhinoscriptsyntax as rs
+from EnneadTab import ERROR_HANDLE, LOG
+
+@LOG.log(__file__, __title__)
+@ERROR_HANDLE.try_catch_error()
+def batch_export_rhino_view():
+    res = rs.PropertyListBox(["Width", "Height",  "Format"],
+                             ["1200", "700",  "png"])
+    width = int(res[0])
+    height = int(res[1])
+    format = res[2]
+    
+
+    folder = rs.BrowseForFolder("Select a folder to save the images")
+    if not folder:
+        return
+    
+    all_views = rs.NamedViews()
+
+    for view in sorted(all_views, reverse=True):
+        print("Exporting view [{}]".format(view))
+        rs.RestoreNamedView(view, view=None, restore_bitmap=False)
+        file_path = "{}\\{}.{}".format(folder, view, format)
+        rs.ViewCapture("!_-ViewCaptureToFile _Width {} _Height {} _Browse {} _Enter".format(width, height, file_path))
+    
+if __name__ == "__main__":
+    batch_export_rhino_view()
