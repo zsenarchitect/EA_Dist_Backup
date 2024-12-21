ac2fc4911e938286050e65b533cbcdb73ff8ab22
Sen Zhang <zsenarchitect@gmail.com>
2024-12-21 14:05:42 -0500

    Auto Commit 2024-12-21_14-05-41

--- Apps/_revit/EnneaDuck.extension/Ennead.tab/Tools.panel/generic_healthcare_tool.pushbutton/generic_healthcare_tool_script.py
+++ Apps/_revit/EnneaDuck.extension/Ennead.tab/Tools.panel/generic_healthcare_tool.pushbutton/generic_healthcare_tool_script.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -73,3 +74,54 @@ if __name__ == "__main__":
     
 
 
+=======
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+__doc__ = "This will be the foundation for all future health care tools"
+__title__ = "HealthCare\nHelper"
+__tip__ = True
+# from pyrevit import forms #
+from pyrevit import script #
+from pyrevit import forms #
+
+import proDUCKtion # pyright: ignore 
+proDUCKtion.validify()
+from EnneadTab.REVIT import REVIT_APPLICATION
+from EnneadTab import USER, ERROR_HANDLE, LOG
+from Autodesk.Revit import DB # pyright: ignore 
+# from Autodesk.Revit import UI # pyright: ignore
+UIDOC = REVIT_APPLICATION.get_uidoc()
+DOC = REVIT_APPLICATION.get_doc()
+            
+
+
+
+@LOG.log(__file__, __title__)
+@ERROR_HANDLE.try_catch_error()
+def generic_healthcare_tool(doc, show_log):
+
+    options = {"Detail DGSF Chart Update": dgsf_chart_update}
+
+    select_option = forms.SelectFromList.show(options.keys(), multiselect=False, title="Select option")
+    if select_option is None:
+        return
+
+    func = options[select_option]
+    func(doc)
+    
+def dgsf_chart_update(doc):
+    pass
+################## main code below #####################
+output = script.get_output()
+output.close_others()
+
+
+if __name__ == "__main__":
+    generic_healthcare_tool(DOC, show_log=True)
+    
+
+
+>>>>>>> f4887017 (Auto Commit 2024-12-21_14-05-41)
