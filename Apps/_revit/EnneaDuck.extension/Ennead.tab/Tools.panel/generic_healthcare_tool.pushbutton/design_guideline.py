884f14a2cc5b5ca96662f4e0fbbd44fdf3f28170
Sen Zhang <zsenarchitect@gmail.com>
2025-02-05 13:10:08 -0500

    Auto Commit 2025-02-05_13-10-08

--- Apps/_revit/EnneaDuck.extension/Ennead.tab/Tools.panel/generic_healthcare_tool.pushbutton/design_guideline.py
+++ Apps/_revit/EnneaDuck.extension/Ennead.tab/Tools.panel/generic_healthcare_tool.pushbutton/design_guideline.py
@@ -5,11 +5,25 @@ NYU Health as sample
 
 base of design need to have PIM #
 
-need to have "GFA Scheme" and "DGSF Scheme", each need to have a color scheme???
+need to have 
+"GFA Scheme"[Area Scheme]: Tracing of outline on each level, single area per level.
+"DGSF Scheme"[Room Based]: Use room for area. Use rooom seperation line as bounding element in SD stage, and wall as bounding element in DD stage
+
+
 area para name list
 
 
+room para name list
+
+
 parking para name list
+
+
+essential family list:
+    - elevator
+    - elevator door
+    - parking stall
+    - patient bed room planner
 
 
 during early design, avoid using design option for total massing study. Such as T building VS L building. 
