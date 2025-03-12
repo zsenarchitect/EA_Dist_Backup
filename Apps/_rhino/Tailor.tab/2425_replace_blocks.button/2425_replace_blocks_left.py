728257f7a182408cdb734db2136f9f08385b108d
Sen Zhang <zsenarchitect@gmail.com>
2025-03-12 12:12:27 -0400

    Auto Commit 2025-03-12_12-12-26

--- Apps/_rhino/Tailor.tab/2425_replace_blocks.button/2425_replace_blocks_left.py
+++ Apps/_rhino/Tailor.tab/2425_replace_blocks.button/2425_replace_blocks_left.py
@@ -8,17 +8,27 @@ from EnneadTab.RHINO import RHINO_BLOCK
 @LOG.log(__file__, __title__)
 @ERROR_HANDLE.try_catch_error()
 def replace_blocks():
-    options = ["twinmotion block --> enscape block",
-               "enscape block --> twinmotion block"]
+    options = ["Twinmotion block --> Enscape block",
+               "Enscape block --> Twinmotion block"]
     res = rs.ListBox(options, "How to handle block replacement?", "Select option")
     if res == options[0]:
-        data = EXCEL.read_data_from_excel("J:\\2425\\0_3D\\03_Enscape\\enscape_twinmotion_block_mapping.xlsx",
+        normal_direction = False
+    elif res == options[1]:
+        normal_direction = True
+    else:
+        return
+        
+    data = EXCEL.read_data_from_excel("J:\\2425\\0_3D\\03_Enscape\\enscape_twinmotion_block_mapping.xlsx",
                                             return_dict=False,
                                             worksheet="EnneadTab Helper")
     data = data[1:]
     for row in data:
-        old_block = row[0].get("value")
-        new_block = row[1].get("value")
+        if normal_direction:
+            old_block = row[0].get("value") # getting enscape block
+            new_block = row[1].get("value") # getting twinmotion block
+        else:
+            old_block = row[1].get("value") # getting twinmotion block
+            new_block = row[0].get("value") # getting enscape block
         RHINO_BLOCK.replace_block(old_block, new_block)
     rs.Redraw()
     
