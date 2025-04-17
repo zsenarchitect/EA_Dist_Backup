2ef3e6ded755948e91b86ed4c031caa8a884859a
Sen Zhang <zsenarchitect@gmail.com>
2025-04-17 16:53:52 -0400

    Auto-commit: Update distribution files

--- Apps/_rhino/Modify.tab/move_fixed_dist.button/move_fixed_dist_left.py
+++ Apps/_rhino/Modify.tab/move_fixed_dist.button/move_fixed_dist_left.py
@@ -9,7 +9,8 @@ Features:
 - Safe window handling to prevent UI errors
 - Allows full interaction with Rhino while dialog is open
 - Gamepad-style button layout
-- Zoom to selected function"""
+- Zoom to selected function
+- Choice between View CPlane and World CPlane coordinate systems"""
 
 
 from EnneadTab import ERROR_HANDLE, LOG, DATA_FILE, NOTIFICATION
@@ -19,6 +20,7 @@ import scriptcontext as sc # pyright: ignore
 import Rhino # pyright: ignore
 import Eto # pyright: ignore
 import System # pyright: ignore
+
 
 FORM_KEY = 'move_fixed_dist_modeless_form'
 
@@ -33,11 +35,23 @@ class MoveFixedDistDialog(Eto.Forms.Form):
         # Get saved distance value
         self.distance = DATA_FILE.get_sticky("move_fixed_dist_value", 1.0)
         
+        # Get saved coordinate system preference
+        self.use_view_cplane = DATA_FILE.get_sticky("move_fixed_dist_use_view_cplane", True)
+        
         # Create distance input
         distance_label = Eto.Forms.Label(Text="Distance:")
         self.distance_textbox = Eto.Forms.TextBox(Text=str(self.distance))
         self.distance_textbox.KeyDown += self.on_distance_keydown
         self.distance_textbox.LostFocus += self.on_distance_changed
+        
+        # Create coordinate system radio buttons
+        cplane_label = Eto.Forms.Label(Text="Coordinate System:")
+        self.cplane_radio = Eto.Forms.RadioButtonList()
+        self.cplane_radio.Orientation = Eto.Forms.Orientation.Horizontal
+        self.cplane_radio.Items.Add("View CPlane ")
+        self.cplane_radio.Items.Add("World CPlane ")
+        self.cplane_radio.SelectedIndex = 0 if self.use_view_cplane else 1
+        self.cplane_radio.SelectedIndexChanged += self.on_cplane_changed
         
         # Validation message
         self.validation_label = Eto.Forms.Label(Text="")
@@ -61,10 +75,20 @@ class MoveFixedDistDialog(Eto.Forms.Form):
         
         down_button = Eto.Forms.Button(Text="Down")
         down_button.Click += self.on_down_click
-        
+
         # Create zoom button
         zoom_button = Eto.Forms.Button(Text="Zoom Selected")
         zoom_button.Click += self.on_zoom_click
+        
+        # Create footnote label
+        footnote_label = Eto.Forms.Label()
+        footnote_label.Text = "Note: Red, Green, Blue colors correspond to\nthe XYZ positive axis direction of the CPlane"
+        try:
+            # Try to set a smaller font size
+            footnote_label.Font = Eto.Drawing.Font.FromName("Arial", 6)
+        except:
+            # If that fails, we'll use the default font
+            pass
         
         # Create layout
         layout = Eto.Forms.DynamicLayout()
@@ -73,6 +97,7 @@ class MoveFixedDistDialog(Eto.Forms.Form):
         
         # Add controls to layout
         layout.AddSeparateRow(distance_label, self.distance_textbox)
+        layout.AddSeparateRow(cplane_label, self.cplane_radio)
         layout.AddRow(self.validation_label)
         
         # Create button layout according to sketch
@@ -91,14 +116,20 @@ class MoveFixedDistDialog(Eto.Forms.Form):
         
         layout.AddRow(button_layout)
         
-
-        
         layout.AddSeparateRow(None, up_button, down_button, None)
+        
+        # Add footnote at the bottom
+        layout.AddRow(footnote_label)
         
         self.Content = layout
         
         # Apply EnneadTab dark style
         RHINO_UI.apply_dark_style(self)
+        
+        # Set button text colors after applying dark style
+        north_button.TextColor = Eto.Drawing.Colors.LightGreen
+        east_button.TextColor = Eto.Drawing.Colors.LightCoral
+        up_button.TextColor = Eto.Drawing.Colors.SkyBlue
         
         # Set form closed event handler
         self.Closed += self.OnFormClosed
@@ -124,6 +155,11 @@ class MoveFixedDistDialog(Eto.Forms.Form):
     def on_distance_changed(self, sender, e):
         self.validate_distance()
     
+    def on_cplane_changed(self, sender, e):
+        self.use_view_cplane = self.cplane_radio.SelectedIndex == 0
+        DATA_FILE.set_sticky("move_fixed_dist_use_view_cplane", self.use_view_cplane)
+
+    @ERROR_HANDLE.try_catch_error()
     def move_objects(self, direction_vector):
         # Check if any objects are selected
         selected_objects = rs.SelectedObjects()
@@ -135,30 +171,55 @@ class MoveFixedDistDialog(Eto.Forms.Form):
         if not self.validate_distance():
             return
         
-        # Create movement vector
-        move_vector = rs.VectorScale(direction_vector, self.distance)
+        # Get appropriate coordinate plane based on user selection
+        if self.use_view_cplane:
+            # Get the view CPlane
+            cplane = sc.doc.Views.ActiveView.ActiveViewport.GetConstructionPlane().Plane
+        else:
+            # Use world XY plane
+            cplane = Rhino.Geometry.Plane.WorldXY
+        
+        # Map axis index (0=X, 1=Y, 2=Z) to corresponding plane axis
+        plane_axes = [cplane.XAxis, cplane.YAxis, cplane.ZAxis]
+        
+        # Find which axis has a non-zero value (should only be one)
+        for i in range(3):
+            if direction_vector[i] != 0:
+                # Get the corresponding plane axis and apply the sign
+                direction = plane_axes[i] * direction_vector[i]
+                break
+        
+        # Scale by distance
+        move_vector = rs.VectorScale(direction, self.distance)
         
         # Move objects
         rs.MoveObjects(selected_objects, move_vector)
     
     def on_north_click(self, sender, e):
-        self.move_objects([0, self.distance, 0])
+        """return unitilized vector"""
+        self.move_objects([0, 1, 0])
     
     def on_south_click(self, sender, e):
-        self.move_objects([0, -self.distance, 0])
+        """return unitilized vector"""
+        self.move_objects([0, -1, 0])
     
     def on_east_click(self, sender, e):
-        self.move_objects([self.distance, 0, 0])
+        """return unitilized vector"""
+        self.move_objects([1, 0, 0])
     
     def on_west_click(self, sender, e):
-        self.move_objects([-self.distance, 0, 0])
+        """return unitilized vector"""
+        self.move_objects([-1, 0, 0])
     
     def on_up_click(self, sender, e):
-        self.move_objects([0, 0, self.distance])
+        """return unitilized vector"""
+        self.move_objects([0, 0, 1])
     
     def on_down_click(self, sender, e):
-        self.move_objects([0, 0, -self.distance])
-    
+        """return unitilized vector"""
+        self.move_objects([0, 0, -1])
+
+    @ERROR_HANDLE.try_catch_error()
     def on_zoom_click(self, sender, e):
         # Check if any objects are selected
         selected_objects = rs.SelectedObjects()
